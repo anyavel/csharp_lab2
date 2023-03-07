260f46ad37992a326664b96c8864b4dac2d4a802
anyavel <99765639+anyavel@users.noreply.github.com>
2023-03-08 01:04:23 +0200

    Add files via upload

---
+++ lab2/Message.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace a2
+{
+    class Message : IComparable
+    {
+        private string senderNumber;
+        private string receiverNumber;
+        private string text;
+        private uint price;
+        public Message()
+        {
+            senderNumber = "+3800000000";
+            receiverNumber = "+3800000000";
+            text = "";
+            price = 0;
+        }
+        public Message(string sNum, string rNum, string txt)
+        {
+            senderNumber = sNum;
+            receiverNumber = rNum;
+            text = txt;
+            calculatePrice();
+        }
+        public void calculatePrice()
+        {
+            price = (uint)text.Length / 40 + 1;
+        }
+        public override string ToString()
+        {
+            return $"Receiver: {receiverNumber}. Sender: {senderNumber}\nText: {text}\nPrice:{price} hrn.";
+        }
+        public int CompareTo(object obj)
+        {
+            Message msg = obj as Message;
+            int res = this.text.Length - msg.text.Length;
+            if (res == 0)
+            {
+                return 0;
+            }
+            else if (res > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public static bool operator >(Message first, Message second)
+        {
+            return first.CompareTo(second) > 0;
+        }
+        public static bool operator <(Message first, Message second)
+        {
+            return first.CompareTo(second) < 0;
+        }
+        public override bool Equals(object obj)
+        {
+            Message msg = obj as Message;
+            return this.senderNumber == msg.senderNumber && this.receiverNumber == msg.receiverNumber && this.text == msg.text;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Message first, Message second)
+        {
+            return first.Equals(second);
+        }
+        public static bool operator !=(Message first, Message second)
+        {
+            return !first.Equals(second);
+        }
+        public static Message operator +(Message msg, string newText)
+        {
+            return new Message(msg.senderNumber, msg.receiverNumber, msg.text + newText);
+        }
+        public string SenderNumber
+        {
+            get
+            {
+                return senderNumber;
+            }
+            set
+            {
+                senderNumber = value.Length == 10 || value.Length == 13 ? value : senderNumber;
+            }
+        }
+        public string ReceiverNumber
+        {
+            get
+            {
+                return receiverNumber;
+            }
+            set
+            {
+                receiverNumber = value.Length == 10 || value.Length == 13 ? value : receiverNumber;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value.GetType().ToString() == "System.String" ? value : text;
+                calculatePrice();
+            }
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message m1 = new Message("+380967894321", "+380987654321", "fhdsfgsfdhsfhsfghsdfhsdgfhs" +
+                "dfhdsgfjsgfshfsgyreuyghdsgfshxvvghxgvhgdhfgshfgsyffsygdfgfushavsdhvd213132");
+            Message m2 = new Message("+380732141098", "+380501234958", "aaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccceeeeeeeeeeeeeeeeeee" +
+                "fffffffffffffffffffffgggggggggggggggggggggghhhhhhhhhhhhhhhhhhh");
+            Message m3 = new Message("+380980416677", "+380112201298", "ghjkdsbghdbgjnkjkhguwrhonfkeh23k4h23nemnfkjxnvoidpo;klsua8yfw");
+            Message m4 = new Message("+380632258371", "+380664780102", "zxczcsdfdsfk;ovkxlvmdklfhuebfjsfhsdgfhdsbfhsvdfhsdfshdfj" +
+                "gwrwirhuwehriowqrhwrnwerbwbfhjjygwerrrrrrrrrrrrrrrrrdfffffffffffffffasasfasfsfs");
+            Message m5 = new Message("+380735141168", "+380730985060", "haplfsdhbdihggsklmewqlojro823ehirdnfxb vbyusdjs sbghsehkwajnfvjgsakkhnf vsdfgakej");
+            int n = 5;
+            Message[] messages = new Message[n];
+            messages[0] = m1;
+            messages[1] = m2;
+            messages[2] = m3;
+            messages[3] = m4;
+            messages[4] = m5;
+            Console.Write("Add new message? (yes or no): ");
+            string choice = Console.ReadLine();
+            if (choice == "yes")
+            {
+                Message m6 = new Message();
+                Console.Write("Enter sender phone number: ");
+                m6.SenderNumber = Console.ReadLine();
+                Console.Write("Enter receiver phone number: ");
+                m6.ReceiverNumber = Console.ReadLine();
+                Console.Write("Enter Text: ");
+                m6.Text = Console.ReadLine();
+                //Console.WriteLine(m6);
+                Array.Resize(ref messages, n + 1);
+                messages[messages.Length - 1] = m6;
+            }
+            Console.WriteLine("All messages:\n");
+            for (int i = 0; i < messages.Length; ++i)
+            {
+                Console.WriteLine(messages[i]);
+            }
+            Array.Sort(messages);
+            Console.WriteLine($"The longest message:\n{messages[messages.Length - 1]}");
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine($"{i + 1} longest message:\n{messages[messages.Length - i]}");
+            }
+            for (int i = 0; i < messages.Length; ++i)
+            {
+                messages[i] += " Happy New Year!";
+                Console.WriteLine(messages[i]);
+            }
+        }
+    }
+
+}
